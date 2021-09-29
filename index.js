c9cf65df50ed06ed9baf5021122732244257017f
Dieumegard <dieumegard.gregory@gmail.com>
2021-09-29 11:35:14 +0200

    ajout des fichiers pour test npm

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require("chalk");
+
+console.log(chalk.blue('Benji'));
+console.log(chalk.red('Tatiana'));
+console.log(chalk.black('Xavier'));
+console.log(chalk.yellow('Christelle'));
